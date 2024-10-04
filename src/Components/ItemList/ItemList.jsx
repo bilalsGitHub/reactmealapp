7e1fd801cd17973df012f0985dc0f5f4a705876a
Bilal <bilalhinis@gmail.com>
2024-10-04 07:13:49 +0200

    04.10.2024

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -3,16 +3,18 @@ import "./ItemList.css";
 import Item from "../Item/Item";
 const ItemList = ({ items }) => {
   return (
-    <div className="item-list">
+    <div>
       <h2>Item List</h2>
-      {items.map(({ strMeal, strMealThumb, idMeal }) => (
-        <Item
-          key={idMeal}
-          strMeal={strMeal}
-          strMealThumb={strMealThumb}
-          idMeal={idMeal}
-        />
-      ))}
+      <div className="item-list">
+        {items.map(({ strMeal, strMealThumb, idMeal }) => (
+          <Item
+            key={idMeal}
+            strMeal={strMeal}
+            strMealThumb={strMealThumb}
+            idMeal={idMeal}
+          />
+        ))}
+      </div>
     </div>
   );
 };
